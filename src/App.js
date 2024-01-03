c986626f814bb2f850056a817ebfc58d7ecea8e9
yousefnegmeldin <yousefnegm@gmail.com>
2024-01-03 02:19:06 +0200

    added basic first styling and set up material ui

--- src/App.js
+++ src/App.js
@@ -3,28 +3,33 @@ import './App.css';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 import { Actors, Movies, NavBar, MovieInformation, ProfilePage } from './components';
+import useStyles from './styles';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <main>
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
       <NavBar />
-      <Switch>
-        <Route path="/" exact>
-          <Movies />
-        </Route>
-        <Route path="/movie/:id" exact>
-          <MovieInformation />
-        </Route>
-        <Route path="/actors/:id" exact>
-          <Actors />
-        </Route>
-        <Route path="/profile/:id" exact>
-          <ProfilePage />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route path="/" exact>
+            <Movies />
+          </Route>
+          <Route path="/movie/:id" exact>
+            <MovieInformation />
+          </Route>
+          <Route path="/actors/:id" exact>
+            <Actors />
+          </Route>
+          <Route path="/profile/:id" exact>
+            <ProfilePage />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
