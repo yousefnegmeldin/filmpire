0d4a5aa8d376a5483de515197c0af8696955c4a5
yousefnegm <yousefnegm@gmail.com>
2024-01-04 03:58:00 +0200

    added navbar with styles

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({});
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
 
   </React.StrictMode>,
 );
