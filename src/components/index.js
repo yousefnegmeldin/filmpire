3813adfa63e483e588f3e026bacd46f9ff36332f
yousefnegm <yousefnegm@gmail.com>
2024-01-08 23:03:39 +0200

    added singular Movie component and rendered them out inside Movies

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-cycle */
 export { default as Actors } from './Actors/Actors';
 export { default as Movies } from './Movies/Movies';
 export { default as NavBar } from './NavBar/NavBar';
 export { default as MovieInformation } from './MovieInformation/MovieInformation';
 export { default as ProfilePage } from './ProfilePage/ProfilePage';
+export { default as MovieList } from './MovieList/MovieList';
+export { default as Movie } from './Movie/Movie';
