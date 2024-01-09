56d45509b257ad592d4c1d1132c95751f3a26806
yousefnegm <yousefnegm@gmail.com>
2024-01-09 22:39:31 +0200

    added pictures,api fetching for genres, and styled the movies, along with new rating

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -4,6 +4,27 @@ export default makeStyles((theme) => ({
   movie: {
     padding: '10px',
   },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    textDecoration: 'none',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    '&:hover': {
+      cursor: 'pointer',
+      textDecoration: 'none',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
   title: {
     color: theme.palette.text.primary,
     textOverflow: 'ellipsis',
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -8,15 +8,20 @@ export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
-    //* get movies by [type]
+    // get movies by [type]
     getMovies: builder.query({
       query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
     }),
 
     // get movie details by [id]
+
+    // get genres
+    getGenres: builder.query({
+      query: () => `/genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
 export const {
-  useGetMoviesQuery,
+  useGetMoviesQuery, useGetGenresQuery,
 } = tmdbApi;
