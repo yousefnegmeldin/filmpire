f6558021b875fefdd011edc5654c3b760d26d6c8
yousefnegm <yousefnegm@gmail.com>
2024-01-12 04:34:55 +0200

    finalized authentication and showed username

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 import { userSelector } from '../../features/auth';
 
 const ProfilePage = () => {
-  console.log('');
   const { user } = useSelector(userSelector);
-
+  const favoriteMovies = [];
+  const logOut = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+  // Profile : {user.username}
   return (
-    <div>
-      Profile : {user.username}
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logOut}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length
+        ? <Typography variant="h5">Add favorites or watchlist some movies!</Typography>
+        : (
+          <Box>
+            Favorite Movies!
+          </Box>
+        )}
+    </Box>
   );
 };
 
